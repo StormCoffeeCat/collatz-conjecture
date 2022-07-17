4f6fb44c98ad140a2e10b6bedf14515ec6dd1df4
StormCoffeeCat <69400452+StormCoffeeCat@users.noreply.github.com>
2022-07-17 16:40:03 +0100

    seed

--- collatz.py
+++ collatz.py
@@ -3,15 +3,15 @@ import random
 
 # Starting statement
 print('This is a simple Collatz Conjecture path generator. \nThe rules are: \nIf the number is even, divide by two. \nIf the number is odd, multiply by three and add one')
-rand = input('Would you like to choose a starting number (type "c") or would you like a random starting number between 1 and 1,000,000 (type "r")?')
+rand = input('Would you like to choose a seed (type "c") or would you like a random seed between 1 and 1,000,000 (type "r")?')
 rand = rand.lower()
 
-# Determines the starting number
+# Determines the seed
 if rand == 'c':
-    number = int(input('Please type an integer for the starting number'))
+    number = int(input('Please type an integer for the seed'))
 elif rand == 'r':
     number = random.randint(1,1000000)
-    print('Your starting number is '+ str(number))
+    print('Your seed is '+ str(number))
     
     # Solving algorithm
 def solve(number):
@@ -33,7 +33,7 @@ def solve(number):
             iteration = iteration+1
             
         if number == 1:
-            print('Your number went through {0} interations.'.format(iteration))
+            print('Your seed went through {0} interations.'.format(iteration))
             break
 
 solve(number)
